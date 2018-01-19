9815f0e0747ca4d7deb4bebe50ca437c20abf9b2
JanDo42 <JanDo42@protonmail.com>
2018-01-20 00:50:51 +0100

    Third course's assignment KMeans

---
+++ 3-Parallel Programming/3-KMeans/src/main/scala/kmeans/KMeans.scala
@@ -0,0 +1,145 @@
+package kmeans
+
+import scala.annotation.tailrec
+import scala.collection._
+import scala.util.Random
+import org.scalameter._
+import common._
+
+class KMeans {
+
+  def generatePoints(k: Int, num: Int): Seq[Point] = {
+    val randx = new Random(1)
+    val randy = new Random(3)
+    val randz = new Random(5)
+    (0 until num)
+      .map({ i =>
+        val x = ((i + 1) % k) * 1.0 / k + randx.nextDouble() * 0.5
+        val y = ((i + 5) % k) * 1.0 / k + randy.nextDouble() * 0.5
+        val z = ((i + 7) % k) * 1.0 / k + randz.nextDouble() * 0.5
+        new Point(x, y, z)
+      }).to[mutable.ArrayBuffer]
+  }
+
+  def initializeMeans(k: Int, points: Seq[Point]): Seq[Point] = {
+    val rand = new Random(7)
+    (0 until k).map(_ => points(rand.nextInt(points.length))).to[mutable.ArrayBuffer]
+  }
+
+  def findClosest(p: Point, means: GenSeq[Point]): Point = {
+    assert(means.size > 0)
+    var minDistance = p.squareDistance(means(0))
+    var closest = means(0)
+    var i = 1
+    while (i < means.length) {
+      val distance = p.squareDistance(means(i))
+      if (distance < minDistance) {
+        minDistance = distance
+        closest = means(i)
+      }
+      i += 1
+    }
+    closest
+  }
+
+  def classify(points: GenSeq[Point], means: GenSeq[Point]): GenMap[Point, GenSeq[Point]] = {
+    //Group by closest mean
+    val grouped = points.groupBy(findClosest(_, means))
+    
+    //Return a genMap with all initial means as keys, even if no point is close to them
+    means.map( mean => (mean,grouped.getOrElse(mean, GenSeq() )) ).toMap
+  
+  }
+
+  def findAverage(oldMean: Point, points: GenSeq[Point]): Point = if (points.length == 0) oldMean else {
+    var x = 0.0
+    var y = 0.0
+    var z = 0.0
+    points.seq.foreach { p =>
+      x += p.x
+      y += p.y
+      z += p.z
+    }
+    new Point(x / points.length, y / points.length, z / points.length)
+  }
+
+  def update(classified: GenMap[Point, GenSeq[Point]], oldMeans: GenSeq[Point]): GenSeq[Point] = {
+    //update average
+    oldMeans.map(oldMean => findAverage(oldMean, classified(oldMean)) )
+  }
+
+  def converged(eta: Double)(oldMeans: GenSeq[Point], newMeans: GenSeq[Point]): Boolean = {
+    var i=0
+    
+    while(i<oldMeans.length){
+      
+      //if any mean has moved by more than eta => return false
+      if(oldMeans(i).squareDistance(newMeans(i))>eta)
+        return false
+        
+      i += 1
+    }
+    //no mean moved a distance more than eta
+    return true
+  }
+
+  @tailrec
+  final def kMeans(points: GenSeq[Point], means: GenSeq[Point], eta: Double): GenSeq[Point] = {
+    //Classify points
+    val classified=classify(points, means)
+    
+    //Update means
+    val newMeans=update(classified,means)
+    
+    //return the new means if converged, call kmeans with the new means
+    if (converged(eta)(means,newMeans)) newMeans else kMeans(points, newMeans, eta) // your implementation need to be tail recursive
+  }
+}
+
+/** Describes one point in three-dimensional space.
+ *
+ *  Note: deliberately uses reference equality.
+ */
+class Point(val x: Double, val y: Double, val z: Double) {
+  private def square(v: Double): Double = v * v
+  def squareDistance(that: Point): Double = {
+    square(that.x - x)  + square(that.y - y) + square(that.z - z)
+  }
+  private def round(v: Double): Double = (v * 100).toInt / 100.0
+  override def toString = s"(${round(x)}, ${round(y)}, ${round(z)})"
+}
+
+
+object KMeansRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 20,
+    Key.exec.maxWarmupRuns -> 40,
+    Key.exec.benchRuns -> 25,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]) {
+    val kMeans = new KMeans()
+
+    val numPoints = 500000
+    val eta = 0.01
+    val k = 32
+    val points = kMeans.generatePoints(k, numPoints)
+    val means = kMeans.initializeMeans(k, points)
+
+    val seqtime = standardConfig measure {
+      kMeans.kMeans(points, means, eta)
+    }
+    println(s"sequential time: $seqtime ms")
+
+    val partime = standardConfig measure {
+      val parPoints = points.par
+      val parMeans = means.par
+      kMeans.kMeans(parPoints, parMeans, eta)
+    }
+    println(s"parallel time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+
+}
